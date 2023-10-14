f3a50ee393a4ee0406c2139f2ced228cedfe84f0
Martin Renteria <martinrenteria@Martins-MBP.lan>
2023-10-14 13:04:48 -0600

    final commit before submit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const startbtn = document.getElementById("start");
 const quizquestions = document.getElementById("questions");
 var score = 0;
-// var timeleft = 60;
 var QI = 0;
 var highScoreArr = JSON.parse(localStorage.getItem("highscores")) || [];
-const timerElement = document.getElementById('timer');
-const questionTimeInSeconds = 5;
-// let seconds = 30;
+const timerElement = document.getElementById("timer");
+let questionTimeInSeconds = 45;
 let timerInterval;
 let addTimeInSeconds = 10;
+let initials = document.getElementById("initials");
+let addScore = 1;
 
-
+initials.style.display = "none";
 const questionArr = [
   {
     question: "1. There are how many types of data in Javascript?",
@@ -18,7 +18,8 @@ const questionArr = [
     correctAnswer: "7",
   },
   {
-    question:"2. What math function would you use to create a random whole number?",
+    question:
+      "2. What math function would you use to create a random whole number?",
     answers: [
       "Math.floor(Math.random()*10)",
       "Math.random(Math.floor()*10)",
@@ -55,79 +56,77 @@ const questionArr = [
       "if {condition} [execute code]",
       "if (condition) {execute code}",
     ],
-    correctAnswer: 'if (condition) {execute code}',
+    correctAnswer: "if (condition) {execute code}",
   },
   {
     question: "7. Which sign means 'OR'",
-      answers: [ '&&', '\\', '!==', '||', ],
-      correctAnswer: '||',
+    answers: ["&&", "\\", "!==", "||"],
+    correctAnswer: "||",
   },
   {
     question: "8. Which sign means not equal to?",
-      answers: [ '&&', '\\', '!==', '||', ],
-      correctAnswer: '!==',
+    answers: ["&&", "\\", "!==", "||"],
+    correctAnswer: "!==",
   },
   {
     question: "9. What is the proper syntax for the 'getElementById' method?",
-      answers: [ 'getelementbyid{"element"}',
-                 'getElementById("element")',
-                 'getElementById("#element")',
-                 'getelementbyid["element"]',
-  ],
-      correctAnswer: 'getElementById("element")',
+    answers: [
+      'getelementbyid{"element"}',
+      'getElementById("element")',
+      'getElementById("#element")',
+      'getelementbyid["element"]',
+    ],
+    correctAnswer: 'getElementById("element")',
   },
   {
     question: "10. What symbol separates key-value pairs in an object?",
-      answers: [ ',', ';', ':', '{}',
-  ],
-      correctAnswer: ',', 
+    answers: [",", ";", ":", "{}"],
+    correctAnswer: ",",
   },
   {
     question: "11. A method is?",
-      answers: [ 'A variable inside an object',
-                'A function inside an object',
-                'A function inside a variable',
-                'A global variable',
-  ],
-      correctAnswer: 'A function inside an object', 
+    answers: [
+      "A variable inside an object",
+      "A function inside an object",
+      "A function inside a variable",
+      "A global variable",
+    ],
+    correctAnswer: "A function inside an object",
   },
   {
     question: "12. A global variable is?",
-      answers: [ 'A variable inside an object',
-                'A variable that can be used anywhere in the world',
-                'A variable that can be accessed from anywhere in the program',
-                'A local variable',
-  ],
-      correctAnswer: 'A variable that can be used from anywhere in the program', 
+    answers: [
+      "A variable inside an object",
+      "A variable that can be used anywhere in the world",
+      "A variable that can be accessed from anywhere in the program",
+      "A local variable",
+    ],
+    correctAnswer: "A variable that can be used from anywhere in the program",
   },
   {
     question: "14. What symbol is used to hold data in an Array?",
-      answers: [ '{}', '()', '//', '[]',
-  ],
-      correctAnswer: '[]',
+    answers: ["{}", "()", "//", "[]"],
+    correctAnswer: "[]",
   },
   {
     question: "15. What symbol is used to comment out multiple lines of code?",
-      answers: [ '//', '\\', '/*', '*/', 'both c and d'
-  ],
-      correctAnswer: 'both c and d',
+    answers: ["//", "\\", "/*", "*/", "both c and d"],
+    correctAnswer: "both c and d",
   },
 ];
 startbtn.addEventListener("click", startQuiz);
-// quizquestions.addEventListener('click', generatequestions);
 
 function startQuiz() {
-  startbtn.style.display = 'none';
+  startbtn.style.display = "none";
+  initials.style.display = 'none';
   displayQuestion(questionTimeInSeconds);
-  startTimer(questionTimeInSeconds);
+  startTimer();
 }
-
-
 
 function displayQuestion(remainingTime) {
   quizquestions.textContent = questionArr[QI].question;
   var buttonBox = document.getElementById("btn-box");
-  buttonBox.innerHTML = '';
+  buttonBox.innerHTML = "";
   questionArr[QI].answers.forEach((answer) => {
     var btn = document.createElement("button");
     btn.textContent = answer;
@@ -136,12 +135,11 @@ function displayQuestion(remainingTime) {
       if (this.value === questionArr[QI].correctAnswer) {
         ++score;
         ++QI;
-        remainingTime += addTimeInSeconds;
-        // startTimer(remainingTime);
-        console.log(remainingTime)
-        // timerElement.textContent = remainingTime;
+        questionTimeInSeconds += addTimeInSeconds;
+        console.log(remainingTime);
       } else {
         ++QI;
+        questionTimeInSeconds -= addTimeInSeconds;
       }
       if (QI === questionArr.length) {
         endGame();
@@ -152,32 +150,40 @@ function displayQuestion(remainingTime) {
     buttonBox.appendChild(btn);
   });
 }
-function startTimer(seconds) {
-  let remainingTime = seconds;
-    timerElement.textContent = 'Time Remaining: ' + remainingTime + ' seconds';
-  
-    timerInterval = setInterval(() => {
-      remainingTime--;
-      timerElement.textContent = 'Time Remaining: ' + remainingTime + ' seconds';
-      
-  
-      if (remainingTime <= 0) {
-        ++QI;
-        clearInterval(timerInterval);
-        displayQuestion(remainingTime);
-        startTimer(questionTimeInSeconds);
-      }
-    }, 1000);
+function startTimer() {
+  timerInterval = setInterval(() => {
+    questionTimeInSeconds--;
+    timerElement.textContent =
+      "Time Remaining: " + questionTimeInSeconds + " seconds";
+
+    if (questionTimeInSeconds <= 0) {
+      ++QI;
+      clearInterval(timerInterval);
+      displayQuestion(questionTimeInSeconds);
+      startTimer();
+      endGame();
+    }
+  }, 1000);
+}
+
+function dislayScore() {
+  document.getElementById('score');
+  score.textContent = score;
+  score.setAttribute('value', score);
+  if(this.value === questionArr[QI].correctAnswer) {
+    ++addScore;
   }
-      
-  
+}
 function endGame() {
-  document.getElementById("quiz-box").innerHTML = "";
-  var myscore = 10;
-  var initials = "mrr";
+  clearInterval(timerInterval);
+  document.getElementById("quiz-box", ).innerHTML = "";
+  console.log('input');
+  var myscore = "";
+  var initials = "";
+  var initialsValue = initials.value;
   var scoreobject = {
     myscore,
-    initials,
+    initials: initialsValue,
   };
   highScoreArr.push(scoreobject);
   localStorage.setItem("highscores", JSON.stringify(highScoreArr));
@@ -185,15 +191,3 @@ function endGame() {
 highScoreArr.sort((a, b) => {
   return b.myscore - a.myscore;
 });
-
-// let remainingTime = questionTimeInSeconds;
-//     timerElement.textContent = `Time Remaining: ${remainingTime} seconds`;
-//     const timerInterval = setInterval (() => {
-//         remainingTime--;
-//         timerElement.textContent = 'Time Remaining: ' + remainingTime + ' seconds';
-//         if (remainingTime <= 0) {
-//             clearInterval(timerInterval);
-//             displayQuestion(QI + 1);
-//         }
-//     }, 1000);
-
